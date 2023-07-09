4a9468003f2e05236cdf666bd4d9b5fc6f5e8cef
boringx <perkeboring@gmail.com>
2023-07-10 00:42:05 +0300

    Set TODOs

--- src/main/kotlin/analyzer.kt
+++ src/main/kotlin/analyzer.kt
@@ -3,6 +3,14 @@ import java.nio.file.*
 import java.nio.file.attribute.BasicFileAttributes
 import java.util.regex.Pattern
 
+// TODO: Start video from ~ 10 minutes
+// TODO: 1. Add features of how deep analyzer should show dependencies
+// TODO: 1.1 Project dependencies to inner project files
+// TODO: 1.2 1.1 + Dependencies that is used by third-party libraries
+// TODO: 1.3 Std libs and deep of it
+// TODO: 1.4 All of dependencies to the ground
+
+// TODO: 2. Call some renderer library to show graph of dependencies
 fun main() {
     val fileTree = mutableMapOf<String, MutableSet<String>>()
     val srcDirPath = Paths.get("./src/main")
